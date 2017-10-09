129837e276b361d4ee091fff7153324b2e98724e
GUOZF13 <gzf1605471027@gmail.com>
2017-10-09 16:42:12 +0800

    通过Data打开网页或电话本

--- app/src/main/java/com/example/gzfmr/myclass/SecondActivity.java
+++ app/src/main/java/com/example/gzfmr/myclass/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.gzfmr.myclass;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String TAG = "SecondActivity";
     private Button btnBack;
+    private Button btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +30,31 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         btnBack= (Button) findViewById(R.id.btn_back);
         btnBack.setOnClickListener(this);
 
+        btnCreate= (Button) findViewById(R.id.btn_create);
+        btnCreate.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
 
+        Intent intent = null;
+        intent = new Intent();
+
         switch (view.getId()) {
+            case R.id.btn_create:
+
+//                intent.setAction(Intent.ACTION_VIEW);
+
+                intent.setAction(Intent.ACTION_CALL);
+                intent.setData( Uri.parse("tel:15110068534") );
+                startActivity(intent);
+
+                break;
+
+
             case R.id.btn_back:
 
-                Intent intent = null;
-                intent = new Intent();
 
                 intent.putExtra(TAG, "I'm back");
 
